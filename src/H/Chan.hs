1ca84234809c4866ab6628f4993c3791d8de6f42
Karl Voelker <ktvoelker@gmail.com>
2014-01-12 23:07:42 -0800

    Added H.Chan with split channels

---
+++ src/H/Chan.hs
@@ -0,0 +1,49 @@
+
+module H.Chan
+  ( ReadChan()
+  , WriteChan()
+  , RChan(..)
+  , WChan(..)
+  , RWChan()
+  , splitChan
+  , newSplitChan
+  , C.Chan
+  , C.newChan
+  ) where
+
+import qualified Control.Concurrent.Chan as C
+
+import H.Common
+
+newtype ReadChan a = ReadChan (IO a)
+
+newtype WriteChan a = WriteChan (a -> IO ())
+
+class RChan chan where
+  readChan :: chan a -> IO a
+
+class WChan chan where
+  writeChan :: chan a -> a -> IO ()
+
+class (RChan chan, WChan chan) => RWChan chan where
+
+splitChan :: C.Chan a -> (ReadChan a, WriteChan a)
+splitChan chan = (ReadChan (C.readChan chan), WriteChan (C.writeChan chan))
+
+newSplitChan :: IO (ReadChan a, WriteChan a)
+newSplitChan = splitChan <$> C.newChan
+
+instance RChan ReadChan where
+  readChan (ReadChan r) = r
+
+instance WChan WriteChan where
+  writeChan (WriteChan w) = w
+
+instance RChan C.Chan where
+  readChan = C.readChan
+
+instance WChan C.Chan where
+  writeChan = C.writeChan
+
+instance RWChan C.Chan where
+
