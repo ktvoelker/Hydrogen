2aff6c99b4c4bf72012a9fc1cec28e044c955293
Karl Voelker <karl@karlv.net>
2014-11-18 21:54:28 -0800

    Remove version deps

--- src/H/Import/IO.hs
+++ src/H/Import/IO.hs
@@ -1,11 +1,13 @@
 
 module H.Import.IO
-  ( module Data.Conduit
+  ( module Control.Monad.Trans.Resource
+  , module Data.Conduit
   , module Data.Conduit.Text
   , module System.Exit
   , module System.IO
   ) where
 
+import Control.Monad.Trans.Resource
 import Data.Conduit
 import Data.Conduit.Text (encode, decode, utf8)
 import System.Exit (exitSuccess, exitFailure)
