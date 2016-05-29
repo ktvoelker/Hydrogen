ce9ff625b3deb5fc04f7a3dfb8884a9956ad2abf
Karl Voelker <karl@karlv.net>
2016-05-29 10:07:56 -0700

    Export Double and subtract

--- src/H/Prelude/Num.hs
+++ src/H/Prelude/Num.hs
@@ -2,12 +2,12 @@
 module H.Prelude.Num
   ( module Data.Ratio
   , Num(..), Integral(..), Fractional(..), Real(..), RealFrac(..)
-  , Int, Integer, (^)
+  , Int, Integer, (^), Double, subtract
   ) where
 
 import Data.Ratio
 import Prelude
   ( Num(..), Integral(..), Fractional(..), Real(..), RealFrac(..)
-  , Int, Integer, (^)
+  , Int, Integer, (^), Double, subtract
   )
 
